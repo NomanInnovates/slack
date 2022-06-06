a14e73bf660661400e1647a70367bc53b020f064
Farrukh Ehsan <FarrukhE@nfolksdata.com>
2022-06-06 19:13:14 +0500

    Auth complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import App from './components/App'
+// import Login from './components/Auth/Login'
+// import Register from './components/Auth/Register'
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function Main() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route exact path="/" component={App} />
+//           <Route path="/login" component={Login} />
+//           <Route path="/register" component={Register} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default Main;
+
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
 }
 
 export default App;

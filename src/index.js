5e4f14918b101dc4c45bae7d42afda60fe21fc1c
Nomi0125 <nomansandhu0125@gmail.com>
2022-06-24 19:48:34 +0500

    Format change

--- src/index.js
+++ src/index.js
@@ -2,29 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { createStore } from "redux";
-import { Provider ,connect} from "react-redux";
+import { Provider, connect } from "react-redux";
 import "semantic-ui-css/semantic.min.css";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { auth } from './firebase'
 import rootReducer from "./reducers";
-import { setUser,clearUser } from "./actions";
+import { setUser, clearUser } from "./actions";
 import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
 import Spinner from "./Spinner";
 
-const store = createStore(rootReducer,composeWithDevTools())
+const store = createStore(rootReducer, composeWithDevTools())
 
 class Root extends React.Component {
-  componentDidMount() {console.log(this.props)
+  componentDidMount() {
+    console.log(this.props)
     auth.onAuthStateChanged(user => {
-      if (user) {
-        console.log({user})
+      if (user || true) {
+        console.log({ user })
         this.props.setUser(user);
         this.props.history.push("/");
       }
-      else{
+      else {
         this.props.history.push("/login");
         // clear user
         this.props.clearUser()
@@ -41,14 +42,14 @@ class Root extends React.Component {
   }
 }
 const mapStateToProps = state => ({
-  isLoading:state.user.isLoading
+  isLoading: state.user.isLoading
 })
-const RootWithAuth = withRouter(connect(mapStateToProps,{setUser,clearUser})(Root))
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser, clearUser })(Root))
 ReactDOM.render(
-<Provider store={store}>
-<Router>
-  <RootWithAuth /> 
-</Router>
-</Provider>
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>
   , document.getElementById("root"));
 registerServiceWorker();
